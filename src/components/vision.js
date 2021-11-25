b367d0f55aff185e17c745df9c4d6d819a525261
Hideo Daikoku <neildaikoku@gmail.com>
2021-11-26 02:04:09 +0900

    add vision

--- src/components/vision.js
+++ src/components/vision.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "../styles/vision.module.scss";
+import text from '../data/vision.json';
 
-const Vision = (props) => {
+const Vision =()=>{
   return (
     <div className="sectionContainer">
       <div className="titleContainer">
         <h1 className="sectionTitle">
           Vision
         </h1>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}>
+          <StaticImage src='../images/1.png' width={1000} alt='maria-medem' /> 
+        </div>
+        <div className={styles.textContainer}>
+          <h2 className={styles.paragraphTitle}>{text.title}</h2>
+          <section className={styles.section}>
+          <p className={styles.paragraph}>{text.paragraph_1}</p>
+          <p className={styles.paragraph}>{text.paragraph_2}</p>
+          </section>
+        </div>
       </div>
     </div>
   );
