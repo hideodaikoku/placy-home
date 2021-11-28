fed9a6a75f4b37324b4d46c3aca86a39bfdd1f9a
Hideo Daikoku <neildaikoku@gmail.com>
2021-11-29 08:30:58 +0900

    add company profile

--- src/components/company.js
+++ src/components/company.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as styles from "../styles/company.module.scss";
+import textData from '../data/company.json';
 
 const Company = () => {
   return (
@@ -8,6 +9,16 @@ const Company = () => {
       <h1 className="sectionTitle">
         Company
       </h1>
+    </div>
+    <div className={styles.container}>
+      {
+        textData.map((item, idx)=>(
+          <div key={idx} className={styles.row}>
+            <strong className={styles.label}>{item.label}</strong>
+            <p className={styles.value}>{item.value}</p>
+          </div>
+        ))
+      }
     </div>
   </div>
   );
