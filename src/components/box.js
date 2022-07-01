6f134c950f2b9acd7efd3e012e0e7cb993a14ba7
kenjifushida <kenjifushida@gmail.com>
2022-07-01 21:00:57 +0900

    edit box duration and lang-switch anime

--- src/components/box.js
+++ src/components/box.js
@@ -3,23 +3,28 @@ import {animated, useSpring} from "react-spring"
 
 const Box = ({w, i, boxClass, children}) => {
     const [rand, setRand] = useState(true)
+    const randomX = Math.floor(Math.random() * w)
+    const width = Math.floor(Math.random() * (340 - 150) +150)
+    const height = Math.floor(Math.random() * (340 - 150) +150)
 
     const spring = useSpring({
         loop: true,
         to: { x: -350},
         from: {
-            x: rand ? Math.floor(Math.random() * w) : w
+            x: rand ? randomX : w,
         },
-        config: { duration: Math.floor(Math.random() * (11500-7000)+5000) + i*1500 },
-        onRest: () => setRand(false)
+        config: { duration: Math.floor(Math.random() * (10000 - randomX) + randomX*10) },
+        onRest: () => {
+            setRand(false)
+        }
     })
-
+    
     return (
         <animated.div
         style={{
             ...spring,
-            width: Math.floor(Math.random() * (340 - 150) +150),
-            height: Math.floor(Math.random() * (340 - 150) +150),
+            width: width,
+            height: height,
         }}
         className={boxClass}
         key={i}>
